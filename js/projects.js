ac909ab0817429d77127c263fb74b796383bfe5a
sujithktkm <skataka@ncsu.edu>
2016-03-15 12:28:36 -0400

    Added projects and pagination

---
+++ js/projects.js
@@ -0,0 +1,12 @@
+/**
+ * Created by sujithkatakam on 3/15/16.
+ */
+
+function pagination() {
+    var monkeyList = new List('test-list', {
+        valueNames: ['name'],
+        page: 4,
+        plugins: [ ListPagination({}) ]
+    });
+}
+window.onload = pagination;
